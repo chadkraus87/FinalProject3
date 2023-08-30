85df62abe9c788a213fb76a68ed82fd377574b40
Peterksharma <120028593+Peterksharma@users.noreply.github.com>
2023-08-30 13:32:16 -0500

    created order seed data

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -116,6 +116,7 @@ const typeDefs = gql`
 
   type Query {
     getAllUsers: [User!]!
+    adminGetAllOrders: [Order!]!
     isAdmin: Boolean
     userById(userId: ID!): User
     me: User
@@ -155,78 +156,3 @@ const typeDefs = gql`
 
 module.exports = typeDefs;
 
-//backup of working file
-// const { gql } = require('apollo-server-express');
-
-// const typeDefs = gql`
-//   type Product {
-//     _id: ID!
-//     name: String!
-//     animalType: String!
-//     sizes: [String!]!
-//     colors: [String!]!
-//     description: String!
-//     model: String
-//     price: Float!
-//     reviews: [Review]
-//   }
-
-//   type Review {
-//     user: String!
-//     rating: Float!
-//     text: String
-//     date: String!
-//   }
-  
-
-//   type Profile {
-//     _id: ID
-//     name: String
-//     email: String
-//     password: String
-//   }
-
-//   type Auth {
-//     token: ID!
-//     profile: Profile
-//   }
-
-//   type Order {
-//     _id: ID!
-//     products: [Product]!
-//     orderDate: String!
-//   }
-
-//   input CreateProductInput {
-//     productType: String!
-//     animalType: String!
-//     size: String!
-//     color: String!
-//     description: String!
-//     threedModel: String
-//     price: Float!
-//   }
-
-//   type Query {
-//     profiles: [Profile]!
-//     profile(profileId: ID!): Profile
-//     me: Profile
-//     getProduct(_id: ID!): Product
-//     getAllProducts: [Product]
-//     isAdmin: Boolean!
-//     getOrder(_id: ID!): Order
-//     getAllOrders: [Order]
-//   }
-
-//   type Mutation {
-//     addProfile(name: String!, email: String!, password: String!): Auth
-//     login(email: String!, password: String!): Auth
-
-//     removeProfile: Profile
-//     createProduct(productdata: CreateProductInput!): Product
-
-//     # You can add mutations related to orders here if needed
-//   }
-// `;
-
-// module.exports = typeDefs;
